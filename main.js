45930c9dddba7d58c3790351af91ae8a278ebefb
kirushamatvieiev008 <mkiril087@gmail.com>
2025-12-30 15:57:14 +0200

    gh

---
+++ main.js
@@ -0,0 +1,201 @@
+const catalog = [
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+     photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+  },
+];
+
+// console.log(catalog.length);
+
+let toPushInner = [];
+
+catalog.forEach((goldpeace) => {
+  toPushInner.push(`<li>
+    <img class="imgShowlist" src="${goldpeace.photo}" alt="#" />
+    <h2 class="descriptionList">
+      ${goldpeace.name}
+    </h2>
+    <div class="flexItem">
+      <p>Курс покупки</p>
+      <p>${goldpeace.buyIngPrice}/г</p>
+    </div>
+    <button class="buyList">Детальніше</button><br />
+    <button class="buyList">Купити</button>
+  </li>`);
+});
+
+// console.log(toPushInner.join(''));
+const listGoldCatalog = document.querySelector('.listGoldCatalog');
+
+listGoldCatalog.innerHTML = toPushInner.join('');
