d36e53e5416aec362f9f2b23212e33d5f7bad7e3
kirushamatvieiev008 <mkiril087@gmail.com>
2026-01-01 01:30:53 +0200

    gh

--- comodity.js
+++ comodity.js
@@ -3,180 +3,213 @@ const catalog = [
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 0,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 1,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 2,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 3,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 4,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 5,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 6,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 7,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 8,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 9,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 10,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 11,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 12,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
-  },
-  {
-    name: "Подарунковий золотий зливок Argor-Heraeus 5",
-     photo: "photos/goldTry.jpg",
-    buyIngPrice: "147.20$",
+    id: 13,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 14,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 15,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 16,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 17,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 18,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 19,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 20,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 21,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 22,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 23,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 24,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 25,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 26,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 27,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 28,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 29,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 30,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 31,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 32,
   },
   {
     name: "Подарунковий золотий зливок Argor-Heraeus 5",
     photo: "photos/goldTry.jpg",
     buyIngPrice: "147.20$",
+    id: 33,
+  },
+  {
+    name: "Подарунковий золотий зливок Argor-Heraeus 5",
+    photo: "photos/goldTry.jpg",
+    buyIngPrice: "147.20$",
+    id: 34,
   },
 ];
-
-// console.log(catalog.length);
 
 let toPushInner = [];
 
@@ -190,12 +223,15 @@ catalog.forEach((goldpeace) => {
       <p>Курс покупки</p>
       <p>${goldpeace.buyIngPrice}/г</p>
     </div>
-    <button class="buyList">Детальніше</button><br />
-    <button class="buyList">Купити</button>
+    <a href="makeorder.html" class="buyList">Детальніше</a><br />
+    <a href="makeorder.html" class="buyList">Купити</a>
   </li>`);
 });
 
-// console.log(toPushInner.join(''));
-const listGoldCatalog = document.querySelector('.listGoldCatalog');
+const buyBtn = document.querySelector('.buy');
 
-listGoldCatalog.innerHTML = toPushInner.join('');
+const listGoldCatalog = document.querySelector(".listGoldCatalog");
+
+listGoldCatalog.innerHTML = toPushInner.join("");
+
+// console.log(toPushInner.join(''));
